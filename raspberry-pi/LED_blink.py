0829c8d6fef80d6e5df17ddd2ce066b31d4ecca5
nbednar2929 <nbednar2929@charlottesvilleschools.org>
2023-09-05 10:35:36 -0400

    install!

---
+++ raspberry-pi/LED_blink.py
@@ -0,0 +1,12 @@
+import time
+import board # type: ignore
+import digitalio # type: ignore
+
+led = digitalio.DigitalInOut(board.GP0)
+led.direction = digitalio.Direction.OUTPUT
+
+while True: 
+    led.value = True
+    time.sleep(.5)
+    led.value =  False
+    time.sleep(.5)
