d7640eb42c31e6fa1334d5bf96285423b5ceb56b
nbednar2929 <nbednar2929@charlottesvilleschools.org>
2023-09-18 10:30:39 -0400

    crash avoidance

---
+++ raspberry-pi/Crash_Avoidance.py
@@ -0,0 +1,21 @@
+#type: ignore
+import time
+import board 
+import digitalio 
+import pwmio
+from adafruit_motor import servo
+from digitalio import DigitalInOut,Direction,Pull
+import adafruit_mpu6050
+import busio
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(scl_pin, sda_pin)
+
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True: 
+    print("X Acceleration: " + (str(mpu.acceleration[0])))
+    print("Y Acceleration: " + (str(mpu.acceleration[1])))
+    print("Z Acceleration: " + (str(mpu.acceleration[2])))    
+    time.sleep(0.2)
