8a42fca2de4312061f623747419e1a27ba4a6ea5
nbednar2929 <nbednar2929@charlottesvilleschools.org>
2023-09-20 10:25:55 -0400

    crash avoidance part 1 and 2

--- raspberry-pi/Crash_Avoidance1.py
+++ raspberry-pi/Crash_Avoidance1.py
@@ -1,5 +1,5 @@
 #type: ignore
-import time
+import time #imports 
 import board 
 import digitalio 
 import pwmio
@@ -8,12 +8,13 @@ from digitalio import DigitalInOut,Direction,Pull
 import adafruit_mpu6050
 import busio
 
-sda_pin = board.GP14
+sda_pin = board.GP14 #wiring up accelerometer
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 
-mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu = adafruit_mpu6050.MPU6050(i2c) #initializing MPU
 
+#printing acceleration values
 while True: 
     print("X Acceleration: " + (str(mpu.acceleration[0])))
     print("Y Acceleration: " + (str(mpu.acceleration[1])))
