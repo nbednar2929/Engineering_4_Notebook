9faf0d6e46baf4743c0725a8bbb63696761d5ada
nbednar2929 <nbednar2929@charlottesvilleschools.org>
2023-11-02 10:45:38 -0400

    morse code part 1

--- raspberry-pi/Morse_Code_Part1.py
+++ raspberry-pi/Morse_Code_Part1.py
@@ -1,5 +1,8 @@
-import time
+#type: ignore
+#imports
+import time 
 
+#morse code library
 MORSE_CODE = { 'A':'.-', 'B':'-...',
     'C':'-.-.', 'D':'-..', 'E':'.',
     'F':'..-.', 'G':'--.', 'H':'....',
@@ -14,7 +17,20 @@ MORSE_CODE = { 'A':'.-', 'B':'-...',
     '7':'--...', '8':'---..', '9':'----.',
     '0':'-----', ', ':'--..--', '.':'.-.-.-',
     '?':'..--..', '/':'-..-.', '-':'-....-',
-    '(':'-.--.', ')':'-.--.-'}
+    '(':'-.--.', ')':'-.--.-', ' ':'/'}
 
 while True: 
-    MORSE_CODE["B"]
+    #lowercases letters
+    message = input("Your Message: ").upper()
+    #defins message
+    tmessage = ""
+    #exits code if "-q" is tpyed
+    if "-Q" in message:
+        break
+    #for loop with message
+    for letter in range(len(message)):
+        #converts letters to morse code
+        tmessage += MORSE_CODE[message[letter]] + " "
+    #prints message
+    print(f"Your Translation: {tmessage}")
+    time.sleep(1)
