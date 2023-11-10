fe3bf4c363ce4ae41ba3ec4b560ca2badca15dd6
nbednar2929 <nbednar2929@charlottesvilleschools.org>
2023-11-10 10:12:36 -0500

    data part 1 (AHHHHHHHHH MY PICO WONT CONNECT)

--- raspberry-pi/Data_Part1.py
+++ raspberry-pi/Data_Part1.py
@@ -14,19 +14,19 @@ Green.direction = digitalio.Direction.OUTPUT
 sda_pin = board.GP14 #wiring up accelerometer
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
-
 mpu = adafruit_mpu6050.MPU6050(i2c) #initializing MPU
 
-#printing acceleration values
-while True: 
-    print("X Acceleration: " + (str(mpu.acceleration[0])))
-    print("Y Acceleration: " + (str(mpu.acceleration[1])))
-    print("Z Acceleration: " + (str(mpu.acceleration[2])))    
-    print("Rotation: " + (str(mpu.gyro)))
-    print("")
-    time.sleep(1)
-    if mpu.acceleration[0] >= 9 or mpu.acceleration[1] >= 9: #if gravity affects tilt 
-        Green.value = True #led on
-    elif mpu.acceleration[0] <= -9 or mpu.acceleration[1] <= -9: #if gravity tilt other way
-            Green.value = True
-    else: Green.value = False #else led false
+with open("/data.csv", "a") as datalog:
+    #printing acceleration values
+    while True: 
+        print("X Acceleration: " + (str(mpu.acceleration[0])))
+        print("Y Acceleration: " + (str(mpu.acceleration[1])))
+        print("Z Acceleration: " + (str(mpu.acceleration[2])))    
+        print("Rotation: " + (str(mpu.gyro)))
+        print("")
+        time.sleep(1)
+        if mpu.acceleration[0] >= 9 or mpu.acceleration[1] >= 9: #if gravity affects tilt 
+            Green.value = True #led on
+        elif mpu.acceleration[0] <= -9 or mpu.acceleration[1] <= -9: #if gravity tilt other way
+                Green.value = True
+        else: Green.value = False #else led false
